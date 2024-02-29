98b1908b69afbd5820df11244d9d59744e95d85c
Ashley-Thean <70069771+Ashleyyyt@users.noreply.github.com>
2024-02-29 17:10:47 +0000

    Added helper functions for model

--- C_Models/logReg_TBC.c
+++ C_Models/logReg_TBC.c
@@ -23,21 +23,48 @@ double arg_min(double arr[]){
     return min;
 }
 
-void weights(double *weight_matrix, int n){
+double** transpose(double** matrix){
+    int c = length(matrix);
+    int r = length(matrix[0]); 
+    for(int i=0; i<r; i++){  
+        for(int j=0; j<c; j++){  
+            int temp = matrix[i][j];  
+            matrix[i][j] = matrix[j][i];  
+            matrix[j][i] = temp;  
+        }  
+    }  
+    return matrix;
+}
 
+double weighted_sum_of_squares(double *y, double *X, double *weights, double *beta, int n, int p) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        double y_pred = 0.0;
+        for (int j = 0; j < p; j++) {
+            y_pred += X[i * p + j] * beta[j];
+        }
+        double residual = y[i] - y_pred;
+        sum += weights[i] * residual * residual;
+    }
+    return sum;
+}
+
+
+
+double[] diagonAlleyMatrix(){
+    
+}
+
+
+//w_(k+1) = (transpose(T) * S_k * X)^-1 * transpose(T) * (S_k * X * W_k + y - miu_k)
+void weights(double *weight_matrix, int n){
     node_t * head = NULL;
     head = (node_t *) malloc(sizeof(node_t));
     if (head == NULL) {
         return 1;
     }
-    head->val = 1;
-    head->next = (node_t *) malloc(sizeof(node_t));
-    head->next->val = 2;
-    head->next->next = NULL;
 
-    head->next = NULL;
-
-    *weight_matrix = 
+    //*weight_matrix = 
 }
 
 double coeff(){
